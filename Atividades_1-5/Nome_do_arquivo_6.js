e0fae8fa80c87ba762af626dace1fc5ebb2dff9d
duda sadino <dudababy27@gmail.com>
2022-02-03 11:55:33 -0300

    Exercicio 6 variaveis implementadas

--- Atividades_1-5/Nome_do_arquivo_6.js
+++ Atividades_1-5/Nome_do_arquivo_6.js
@@ -1,7 +1,37 @@
+function ForteTortilha ( profissao, moeda, Dia ){
+      let trabalhador = profissao
+      let Ouro = moeda* Dia
+      let Prata = moeda* Dia
+      let Cobre = moeda* Dia
 
-function ForteTortilha ( Ouro, Prata, Cobre, Dia ){
-   let ferreiro1 = 
-   //let ferreiro2 = 
+
+          return "em " + Dia + " dias o " + profissao + " recebe em Ouro " +  Ouro
+ 
+    }
+console.log(ForteTortilha("carteiro" ,5 , 30  ))
+console.log(ForteTortilha("ferreiro" ,2 , 60  ))
+console.log(ForteTortilha("alfaiate" ,6 , 40  ))
+    /*Exemplo da questao 6
+
+    entrada:
+    Função: Ferreiro
+    Custo: 2 Cobres/dia
+    
+    saida:
+    Salário: em 20 dias, deve se pagar a 3 ferreiros 60 cobres
+    
+    uma moeda de ouro = 10 de prata
+    uma moeda de prata = 10 cobre
+
+    uma moeda de ouro = 10 reais (10 moedas de 1 real)
+    uma moeda de prata = 1 real (10 moedas de 10 centav.)
+    uma moeda de cobre = 10 centavo. (2 moedas de 5 centav.)*/
+    
+   
+   
+   
+   
+    //let ferreiro2 = 
    //let ferreiro3 = 
    
    //let salario = ( Ouro * Dia)
@@ -14,8 +44,8 @@ function ForteTortilha ( Ouro, Prata, Cobre, Dia ){
 //let ferreiro = 
 
 //if (semaforo == "verde") {
-    mensagem = "Pode passar";
-}
+ //   mensagem = "Pode passar";
+//}
 
 
 
